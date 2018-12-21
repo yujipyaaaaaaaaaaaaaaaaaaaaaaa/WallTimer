883feb8f370cb0f9ad68ba5b7304837ac4eaf0dc
yuji nunome <nunome@kousokuya.co.jp>
2018-12-21 15:41:23 +0900

    Timer機能を作った

--- src/walltimer.hpp
+++ src/walltimer.hpp
@@ -3,23 +3,37 @@
 
 #include <chrono>
 #include <map>
+#include <fstream>
 #include <string>
 #include <sstream>
 #include <iostream>
 #include <vector>
 
-
 class WallTimer{
 private:
-  static WallTimer wt;
+  static WallTimer *wt;
 
   std::vector<std::chrono::system_clock::time_point> rapTime;
   std::map<std::string, std::vector<std::chrono::system_clock::time_point>> funcTime;
   WallTimer(){ }
 
 public:
+  ~WallTimer(){
+    if(wt != nullptr)
+    {
+      delete wt;
+    }
+  }
+
   // get singlton instance
-  static WallTimer & GetInstance(){ return wt; }
+  static WallTimer & GetInstance()
+  {
+    if(wt == nullptr)
+    {
+      wt = new WallTimer();
+    }
+    return *wt;
+  }
 
   // RapTime vector clear
   void ClearRapTime()
@@ -36,24 +50,94 @@ public:
     funcTime.clear();
   }
 
-  void RapTimeStuck()
+  void FuncTimeStack(std::string func_name)
+  {
+    // 要素がある場合は1、ない場合は0
+    if(funcTime.count(func_name)){
+      funcTime[func_name].push_back(std::chrono::system_clock::now());
+    }
+    else
+    {
+      std::vector<std::chrono::system_clock::time_point> second;
+      second.push_back(std::chrono::system_clock::now());
+      funcTime.insert(std::make_pair(func_name, second));
+
+    }
+  }
+
+  void OutputFuncTime(std::string filename="")
+  {
+    std::ostringstream ost;
+    for(auto &func : funcTime){
+      auto &timeVec = func.second;
+
+      int64_t total = 0;
+      uint   called = 0;
+
+      for(size_t i = 1; i < timeVec.size(); i += 2){
+        auto count = std::chrono::duration_cast<std::chrono::nanoseconds>(timeVec[i - 0] - timeVec[i - 1]).count();
+        total += count;
+        ++called;
+      }
+      ost << "function name, " << func.first << ", total, " << total << ", called, " << called << ", average, " << total / called << std::endl;
+    }
+
+
+    ost << std::endl;
+    std::string outstr = ost.str();
+    std::cout << outstr;
+    if(filename != ""){
+      std::ofstream elapsedlogger;
+      elapsedlogger.open(filename, std::ios::app);
+      elapsedlogger << outstr;
+      elapsedlogger.close();
+    }
+
+  }
+
+  void RapTimeStack()
   {
     rapTime.push_back(std::chrono::system_clock::now());
   }
 
-  void OutputRapTime(uint64 div=1, std::string filename="")
+  void OutputRapTime(std::string filename="")
   {
     std::ostringstream ost;
-    for (size_t i = 1; i < funcTime.size(); ++i) {
-      ost << std::chrono::duration_cast<std::chrono::nanoseconds>(timevec[i - 0] - timevec[i - 1]).count() << ",";
+    for (size_t i = 1; i < rapTime.size(); ++i) {
+      auto count = std::chrono::duration_cast<std::chrono::nanoseconds>(rapTime[i - 0] - rapTime[i - 1]).count();
+      ost << count << ",";
     }
     ost << std::endl;
     std::string outstr = ost.str();
     std::cout << outstr;
 
+    if(filename != ""){
+      std::ofstream elapsedlogger;
+      elapsedlogger.open(filename, std::ios::app);
+      elapsedlogger << outstr;
+      elapsedlogger.close();
+    }
   }
 
 };
+WallTimer *WallTimer::wt = nullptr;
 
+class FuncTimer{
+private:
+  FuncTimer() = delete;
+  std::string func;
+
+public:
+  FuncTimer(std::string func_name){
+    func = func_name;
+    WallTimer::GetInstance().FuncTimeStack(func);
+  }
+  ~FuncTimer(){
+    WallTimer::GetInstance().FuncTimeStack(func);
+  }
+};
+
+#define FUNC_TIMER auto temp8234901684 = FuncTimer(__FUNCTION__);
+#define RAP_TIMER WallTimer::GetInstance().RapTimeStack();
 
 #endif // _WALL_TIMER_HPP_
