399d3104a8b064a38f08f861c18fc3a64862742e
yuji nunome <yuji.d.is+github@gmail.com>
2019-01-05 13:59:38 +0900

    テストパターンにスレッドを追加した

--- main.cpp
+++ main.cpp
@@ -6,43 +6,44 @@
 void test1()
 {
   FUNC_TIMER;
-  std::this_thread::sleep_for(std::chrono::milliseconds(100));
+  std::this_thread::sleep_for(std::chrono::milliseconds(1));
 }
 
 void test2()
 {
   FUNC_TIMER;
-  std::this_thread::sleep_for(std::chrono::milliseconds(200));
+  std::this_thread::sleep_for(std::chrono::milliseconds(2));
   test1();
 }
 
 void test3()
 {
   FUNC_TIMER;
-  for(int i = 0; i < 2; ++i)
+  for(int i = 0; i < 100; ++i)
   {
-    for(int j = 0; j < 2; ++j)
-    {
-      CLEAR_RAP_TIMER;
-
-      RAP_TIMER;
-      test1();
-      RAP_TIMER;
-      test2();
-      RAP_TIMER;
-
-      std::cout << "trial:" << i << "-" << j << std::endl;
-      // instance access
-      OUTPUT_FUNC_TIME;
-      // macro access
-      OUTPUT_RAP_TIME;
-      std::cout << std::endl;
-    }
-  }
+    RAP_TIMER;
+    test1();
+    RAP_TIMER;
+    test2();
+    RAP_TIMER;
+ }
 }
 
 int main()
 {
   test3();
+
+  OUTPUT_RAP_TIME;
+  OUTPUT_FUNC_TIME;
+  std::cout << std::endl;
+
+  CLEAR_RAP_TIMER;
+  CLEAR_FUNC_TIMER;
+
+  std::thread th1(test3);
+  std::thread th2(test3);
+  th1.join();
+  th2.join();
+
   return 0;
 }
