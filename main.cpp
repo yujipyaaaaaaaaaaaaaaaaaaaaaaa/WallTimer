11c37520a397274e3ab9db4a07c63985f130bf71
yuji nunome <nunome@kousokuya.co.jp>
2019-01-07 13:52:29 +0900

    testコードを変更

--- main.cpp
+++ main.cpp
@@ -29,6 +29,20 @@ void test3()
  }
 }
 
+void test4()
+{
+  FUNC_TIMER;
+  for(int i = 0; i < 100; ++i)
+  {
+    std::thread th1(test1);
+    std::thread th2(test2);
+    th1.join();
+    th2.join();
+ }
+
+
+}
+
 int main()
 {
   test3();
@@ -40,10 +54,10 @@ int main()
   CLEAR_RAP_TIMER;
   CLEAR_FUNC_TIMER;
 
-  std::thread th1(test3);
-  std::thread th2(test3);
+  std::thread th1(test4);
   th1.join();
-  th2.join();
+
+  std::cout << "main:" << std::this_thread::get_id() << std::endl;
 
   return 0;
 }
